58761fc07231810f4f129d6c6666bef4c5002422
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-05-17 17:23:05 +0100

    Update test run files

--- test/unit/server_usage/unit_test_run.sh
+++ test/unit/server_usage/unit_test_run.sh
@@ -5,30 +5,12 @@
 #   is located at.
 
 echo ""
-echo "Unit test:  help_message"
+echo "Unit test:  server_usage.py"
 test/unit/server_usage/help_message.py
-
-echo ""
-echo "Unit test:  get_svr_info"
 test/unit/server_usage/get_svr_info.py
-
-echo ""
-echo "Unit test:  get_svr_mem"
 test/unit/server_usage/get_svr_mem.py
-
-echo ""
-echo "Unit test:  get_proc_mem"
 test/unit/server_usage/get_proc_mem.py
-
-echo ""
-echo "Unit test:  post_process"
 test/unit/server_usage/post_process.py
-
-echo ""
-echo "Unit test:  run_program"
 test/unit/server_usage/run_program.py
-
-echo ""
-echo "Unit test:  main"
 test/unit/server_usage/main.py
 
